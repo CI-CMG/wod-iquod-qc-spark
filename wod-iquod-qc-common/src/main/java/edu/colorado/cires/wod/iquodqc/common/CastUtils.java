fc10b7d4de18f0edb133c968016a20002566a616
Chris Slater <6004777+electricsam@users.noreply.github.com>
2023-09-20 14:55:47 -0600

    get probetype int

--- wod-iquod-qc-common/src/main/java/edu/colorado/cires/wod/iquodqc/common/CastUtils.java
+++ wod-iquod-qc-common/src/main/java/edu/colorado/cires/wod/iquodqc/common/CastUtils.java
@@ -5,11 +5,16 @@ import static edu.colorado.cires.wod.iquodqc.common.CastConstants.PROBE_TYPE;
 import edu.colorado.cires.wod.parquet.model.Attribute;
 import edu.colorado.cires.wod.parquet.model.Cast;
 import java.util.Optional;
+import java.util.OptionalInt;
 
 public final class CastUtils {
 
   public static Optional<Attribute> getProbeType(Cast cast) {
     return cast.getAttributes().stream().filter(a -> a.getCode() == PROBE_TYPE).findFirst();
+  }
+
+  public static OptionalInt getProbeTypeValue(Cast cast) {
+    return getProbeType(cast).map(Attribute::getValue).stream().mapToInt(Double::intValue).findFirst();
   }
 
   private CastUtils() {
