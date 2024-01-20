6772673cedcb5b7ccdae27721f15689d0d3b5ebb
Chris Slater <6004777+electricsam@users.noreply.github.com>
2024-01-19 22:26:26 -0700

    remove duplicate name

--- wod-iquod-qc-check-suite/wod-iquod-qc-check-cotede-spike-gtspp/src/main/java/edu/colorado/cires/wod/iquodqc/check/cotede/spike/CoTeDeSpikeGTSPPCheck.java
+++ wod-iquod-qc-check-suite/wod-iquod-qc-check-cotede-spike-gtspp/src/main/java/edu/colorado/cires/wod/iquodqc/check/cotede/spike/CoTeDeSpikeGTSPPCheck.java
@@ -5,6 +5,11 @@ public class CoTeDeSpikeGTSPPCheck extends CoTeDeSpikeCheck {
   private static final double TEMPERATURE_THRESHOLD = 2.;
 
   @Override
+  public String getName() {
+    return "COTEDE_SPIKE_GTSPP_CHECK";
+  }
+
+  @Override
   protected double getTemperatureThreshold() {
     return TEMPERATURE_THRESHOLD;
   }
