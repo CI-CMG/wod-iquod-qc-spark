cc965b5b76125930b72cd25fe8903e7edaced69a
Chris Slater <6004777+electricsam@users.noreply.github.com>
2024-01-25 09:08:00 -0700

    fix Argo spike check

--- wod-iquod-qc-check-suite/wod-iquod-qc-check-argo-spike/src/main/java/edu/colorado/cires/wod/iquodqc/check/argo/spike/ArgoSpikeCheck.java
+++ wod-iquod-qc-check-suite/wod-iquod-qc-check-argo-spike/src/main/java/edu/colorado/cires/wod/iquodqc/check/argo/spike/ArgoSpikeCheck.java
@@ -7,9 +7,11 @@ import edu.colorado.cires.wod.iquodqc.common.CheckNames;
 import edu.colorado.cires.wod.iquodqc.common.ObsUtils;
 import edu.colorado.cires.wod.parquet.model.Cast;
 import edu.colorado.cires.wod.parquet.model.Depth;
+import edu.colorado.cires.wod.parquet.model.ProfileData;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Optional;
+import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -20,6 +22,15 @@ public class ArgoSpikeCheck extends CommonCastCheck {
     return CheckNames.ARGO_SPIKE_CHECK.getName();
   }
 
+  private static double getPressure(Map<Integer, Double> pressureCache, int depthIndex, double depth, double latitude) {
+    Double pressure = pressureCache.get(depthIndex);
+    if (pressure != null) {
+      return pressure;
+    }
+    pressure = ObsUtils.depthToPressure(depth, latitude);
+    pressureCache.put(depthIndex, pressure);
+    return pressure;
+  }
 
   @Override
   protected Collection<Integer> getFailedDepths(Cast cast) {
@@ -27,25 +38,28 @@ public class ArgoSpikeCheck extends CommonCastCheck {
     Set<Integer> failures = new TreeSet<>();
     double latitude = cast.getLatitude();
 
-    for (int i = 1; i < depths.size()-1; i++) {
-      Optional<Double> prevTemp= getTemperature(depths.get(i-1)).map(v -> v.getValue());
-      Optional<Double> currentTemp= getTemperature(depths.get(i)).map(v -> v.getValue());
-      Optional<Double> nextTemp= getTemperature(depths.get(i+1)).map(v -> v.getValue());
-      if (prevTemp.isPresent() && currentTemp.isPresent() && nextTemp.isPresent()) {
-        double pressure = ObsUtils.depthToPressure(depths.get(i).getDepth(),latitude);
-        double prevT = prevTemp.get();
-        double nextT = nextTemp.get();
-        double spike = Math.abs(currentTemp.get() - (prevT + nextT)/2 - Math.abs((nextT - prevT)/2));
-        if (pressure < 500){
-          if (spike > 6.0){
-            failures.add(i);
-          }
-        } else {
-          if (spike > 2.0){
-            failures.add(i);
-          }
-        }
-      }
+    Map<Integer, Double> pressureCache = new HashMap<>();
+
+    for (int i = 1; i < depths.size() - 1; i++) {
+      final int depthIndex = i;
+      final double depth = depths.get(i).getDepth();
+      getTemperature(depths.get(depthIndex - 1)).map(ProfileData::getValue).ifPresent(prevT -> {
+        getTemperature(depths.get(depthIndex)).map(ProfileData::getValue).ifPresent(currentT -> {
+          getTemperature(depths.get(depthIndex + 1)).map(ProfileData::getValue).ifPresent(nextT -> {
+            double pressure = getPressure(pressureCache, depthIndex, depth, latitude);
+            double spike = Math.abs(currentT - (prevT + nextT) / 2D) - Math.abs((nextT - prevT) / 2D);
+            if (pressure < 500D) {
+              if (spike > 6D) {
+                failures.add(depthIndex);
+              }
+            } else {
+              if (spike > 2D) {
+                failures.add(depthIndex);
+              }
+            }
+          });
+        });
+      });
     }
 
     return failures;
