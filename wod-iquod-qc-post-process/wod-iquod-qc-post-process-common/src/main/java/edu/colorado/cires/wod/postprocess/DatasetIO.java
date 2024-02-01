e774794b56e2c2ad4ef7ec948424f046e4f3c9d2
paytoncain <payton.cain@colorado.edu>
2024-02-01 13:20:09 -0700

    Write to output uri

--- wod-iquod-qc-post-process/wod-iquod-qc-post-process-common/src/main/java/edu/colorado/cires/wod/postprocess/DatasetIO.java
+++ wod-iquod-qc-post-process/wod-iquod-qc-post-process-common/src/main/java/edu/colorado/cires/wod/postprocess/DatasetIO.java
@@ -1,10 +1,8 @@
 package edu.colorado.cires.wod.postprocess;
 
 import java.util.Arrays;
-import org.apache.spark.sql.DataFrameReader;
 import org.apache.spark.sql.DataFrameWriter;
 import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SaveMode;
 import org.apache.spark.sql.SparkSession;
 
@@ -30,7 +28,7 @@ public class DatasetIO {
     if (uri.endsWith(JSON_EXT)) {
       dataFrameWriter.json(uri);
     } else if (uri.endsWith(PARQUET_EXT)) {
-      dataFrameWriter.parquet(PARQUET_EXT);
+      dataFrameWriter.parquet(uri);
     } else {
       throw new IllegalArgumentException("Writing is only supported for json or parquet format");
     }
