9cdf59fd0e0737e2fd4679692780850ea587f651
peemin <peemin.chen@colorado.edu>
2024-11-25 18:14:15 -0700

    WOD-230 update spark version

--- wod-iquod-qc-ospool/src/ospool/wod-iquod-failures-json-spark.sh
+++ wod-iquod-qc-ospool/src/ospool/wod-iquod-failures-json-spark.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+set -ex
 
 year="$1"
 dataset="$2"
@@ -9,9 +9,9 @@ date_folder="$3"
 working_dir="$PWD"
 
 tar -xvf OpenJDK11U-jre_x64_linux_hotspot_11.0.23_9.tar.gz
-tar -xvf spark-3.4.3-bin-hadoop3-scala2.13.tgz
+tar -xvf spark-3.5.3-bin-hadoop3-scala2.13.tgz
 rm OpenJDK11U-jre_x64_linux_hotspot_11.0.23_9.tar.gz
-rm spark-3.4.3-bin-hadoop3-scala2.13.tgz
+rm spark-3.5.3-bin-hadoop3-scala2.13.tgz
 
 tar -xvf resources.tar.gz
 rm resources.tar.gz
@@ -40,7 +40,7 @@ done
 cd "$working_dir"
 
 export JAVA_HOME="$PWD/jdk-11.0.23+9-jre"
-export SPARK_HOME="$PWD/spark-3.4.3-bin-hadoop3-scala2.13"
+export SPARK_HOME="$PWD/spark-3.5.3-bin-hadoop3-scala2.13"
 export PATH="$SPARK_HOME/bin:$JAVA_HOME/bin:$PATH"
 
 mkdir temp
--- wod-iquod-qc-ospool/src/ospool/wod-iquod-qc-spark.sh
+++ wod-iquod-qc-ospool/src/ospool/wod-iquod-qc-spark.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+set -ex
 
 year="$1"
 dataset="$2"
@@ -9,9 +9,9 @@ date_folder="$4"
 working_dir="$PWD"
 
 tar -xvf OpenJDK11U-jre_x64_linux_hotspot_11.0.23_9.tar.gz
-tar -xvf spark-3.4.3-bin-hadoop3-scala2.13.tgz
+tar -xvf spark-3.5.3-bin-hadoop3-scala2.13.tgz
 rm OpenJDK11U-jre_x64_linux_hotspot_11.0.23_9.tar.gz
-rm spark-3.4.3-bin-hadoop3-scala2.13.tgz
+rm spark-3.5.3-bin-hadoop3-scala2.13.tgz
 
 mkdir -p $date_folder/data/qc/$dataset/OBS/$year
 mkdir -p $date_folder/data/parquet/yearly/$dataset/OBS
@@ -37,7 +37,7 @@ for filename in ./*.tar.gz; do
 done
 
 export JAVA_HOME="$PWD/jdk-11.0.23+9-jre"
-export SPARK_HOME="$PWD/spark-3.4.3-bin-hadoop3-scala2.13"
+export SPARK_HOME="$PWD/spark-3.5.3-bin-hadoop3-scala2.13"
 export PATH="$SPARK_HOME/bin:$JAVA_HOME/bin:$PATH"
 
 mkdir temp
